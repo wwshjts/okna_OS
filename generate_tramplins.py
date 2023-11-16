4b4f65282a52d78e6b5a31a1f8ae14f80d81577c
y.kareba <y.kareba@g.nsu.ru>
2023-11-16 22:44:12 +0700

    Generated tramplins

---
+++ generate_tramplins.py
@@ -0,0 +1,10 @@
+with open("idt.c", 'w') as out:
+    for i in range(0xFF + 1):
+        out.write(f'void static tramplin_{i:02X}() {"{"}panic_handler(0x{i:02X});{"}"}\n')
+    arr_str = 'static void** tramplins = {'
+    for i in range(0xFF):
+        arr_str += f'tramplin_{i:02X}, '
+    arr_str += f'tramplin_FF' + "}\n"
+    out.write(arr_str)
+
+    
